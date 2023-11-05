c6234f4f2dae9997a9b51fe6527564d03acb744a
max pleaner <max.pleaner@hover.to>
2023-11-05 09:12:33 -0800

    max out pct

--- server.rb
+++ server.rb
@@ -10,5 +10,4 @@ get '/data' do
   content_type 'application/json'
   headers['Content-Encoding'] = 'gzip'
   send_file 'data/combined.json.gz', type: :json, disposition: 'attachment'
-  # send_file('./data/combined.json.gz', filename: "data.gz")
 end
